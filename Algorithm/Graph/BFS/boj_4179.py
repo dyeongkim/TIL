f40d5199ee5b0895c3a68a4de04f9c22ceb0acb7
dyeongkim <dyeongkim98@gmail.com>
2023-03-07 17:01:11 +0900

    런타임에러 상태 임시저장

--- Algorithm/Graph/BFS/boj_4179.py
+++ Algorithm/Graph/BFS/boj_4179.py
@@ -9,23 +9,54 @@ dx = [0, 1, 0, -1]
 dy = [-1, 0, 1, 0]
 
 maze = []
+dist_j = []
+dist_f = []
+
+res = 0
 for i in range(c):
 	maze.append(list(input()))
+	dist_j.append([])
+	dist_f.append([])
 	for j in range(r):
+		dist_j[i].append(-1)
+		dist_f[i].append(-1)
 		if maze[i][j] == 'J':
-			queue_J.append((0,i,j))
+			queue_J.append((i,j))
+			dist_j[i][j] = 0
 		elif maze[i][j] == 'F':
-			queue_F.append((-1,i,j))
+			queue_F.append((i,j))
+			dist_f[i][j] = 0
 
 def bfs():
+	while queue_F:
+		fy, fx = queue_F.popleft()
+		for i in range(4):
+			ny = dy[i] + fy
+			nx = dx[i] + fx
+			if nx < 0 or nx >= r or ny < 0 or ny >= c :
+				continue
+			if dist_f[ny][nx] >= 0 or maze[ny][nx] == '#':
+				continue
+			dist_f[ny][nx] = dist_f[fy][fx]+1
+			queue_F.append((ny,nx))
 	while queue_J:
 		jy, jx = queue_J.popleft()
 		for i in range(4):
-			jny = dy[i] + jy
-			jnx = dx[i] + jx
-			
-			if maze[jny][jnx] == '#' or maze[jny][jnx] == 'F':
+			ny = dy[i] + jy
+			nx = dx[i] + jx
+			if nx < 0 or nx >= r or ny < 0 or ny >= c :
+				return dist_j[jy][jx] + 1
+			if dist_j[ny][nx] >= 0 or maze[ny][nx] == '#':
 				continue
-		
-			if jnx >= r or jnx < 0 or jny >= c or jny < 0:
-				return
+			if dist_f[ny][nx] != -1 and dist_f[ny][nx] <= dist_j[jy][jx]+1:
+				continue
+			dist_j[ny][nx] = dist_j[jy][jx]+1
+			queue_J.append((ny,nx))
+	return 0
+
+	
+res = bfs()
+if res != 0:
+	print(res)
+else:
+	print('IMPOSSIBLE')
