5553733f2770228f22a030d12dd27a01aded8cd4
dyeongkim <dyeongkim98@gmail.com>
2023-03-20 23:43:19 +0900

    1182 완

--- Algorithm/Backtracking/boj_1182.py
+++ Algorithm/Backtracking/boj_1182.py
@@ -1,7 +1,25 @@
 N, S = map(int,input().split())
 val = list(map(int,input().split()))
+cnt = 0
+arr_sum = 0
 
 def dfs(cur):
+    global cnt, arr_sum
+
     if cur == N:
         return
     
+    if S == arr_sum + val[cur]:
+        cnt += 1
+
+    dfs(cur+1)
+
+    arr_sum += val[cur]
+    dfs(cur+1)
+
+    arr_sum -= val[cur]
+
+
+dfs(0)
+
+print(cnt)
