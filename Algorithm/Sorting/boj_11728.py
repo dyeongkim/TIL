77e4f993961f483f4fc0190f3be6999bbb4ce1b5
dyeongkim <dyeongkim98@gmail.com>
2023-05-04 16:47:16 +0900

    11728 merge sort 추가예정

--- Algorithm/Sorting/boj_11728.py
+++ Algorithm/Sorting/boj_11728.py
@@ -1,2 +1,13 @@
 # 백준 11728 - 배열합치기
+import sys
+
+input = sys.stdin.readline
+
 N,M = map(int,input().split())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+
+result = A+B
+result.sort()
+
+print(*result)
